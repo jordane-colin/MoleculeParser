9a9dd5814fe16911e84e04401b4831008250b68e
Jordane Colin <jordane.colin@gmail.com>
2020-11-08 19:24:57 +0100

    fix lint

--- logger.py
+++ logger.py
@@ -6,9 +6,11 @@ import os
 
 def get_logger():
     # set log level from env variable
-    LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
-    logging.basicConfig(level=LOG_LEVEL)
-    logging.config.fileConfig('./logging.ini', disable_existing_loggers=False, defaults={'level': LOG_LEVEL})
+    log_level = os.environ.get('LOG_LEVEL', 'INFO').upper()
+    logging.basicConfig(level=log_level)
+    logging.config.fileConfig('./logging.ini',
+                              disable_existing_loggers=False,
+                              defaults={'level': log_level})
     logger = logging.getLogger(__name__)
 
     return logger
